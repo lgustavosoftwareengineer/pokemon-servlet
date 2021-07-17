f82d185e09c58223817ab0c5940943c5de136763
Luiz Gustavo <luiz.matias@upe.br>
2021-07-16 21:29:35 -0300

    chore: change the name from repository to repositories

---
+++ src/main/java/com/app/upe/myproject/mypokedexapi/repositories/PokemonsRepository.java
@@ -0,0 +1,45 @@
+package com.app.upe.myproject.mypokedexapi.repositories;
+
+import java.util.ArrayList;
+
+import com.app.upe.myproject.mypokedexapi.models.pokemons.Pokemon;
+
+public class PokemonsRepository extends Repository<Pokemon> {
+
+  @Override
+  public void add(Pokemon element) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public Pokemon find(String searchParam) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Pokemon findById(String id) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void update(String id, Pokemon element) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public Pokemon delete(String id) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ArrayList<Pokemon> getAll() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+}
---
+++ src/main/java/com/app/upe/myproject/mypokedexapi/repositories/Repository.java
@@ -0,0 +1,17 @@
+package com.app.upe.myproject.mypokedexapi.repositories;
+
+import java.util.ArrayList;
+
+public abstract class Repository<T> {
+  public abstract void add(T element);
+  
+  public abstract T find(String searchParam);
+
+  public abstract T findById(String id);
+
+  public abstract void update(String id, T element);
+
+  public abstract T delete(String id);
+
+  public abstract ArrayList<T> getAll();
+}
