f70fd159d758d9ef97798b46ac4b5f0bbb3980ba
Luiz Gustavo <luiz.matias@upe.br>
2021-07-18 14:48:22 -0300

    feat(utils/encryptpassword): add lib to hash the password with sha-256

--- src/main/java/com/app/upe/myproject/mypokedexapi/utils/EncryptPassword.java
+++ src/main/java/com/app/upe/myproject/mypokedexapi/utils/EncryptPassword.java
@@ -1,13 +1,22 @@
 package com.app.upe.myproject.mypokedexapi.utils;
+
+import java.nio.charset.StandardCharsets;
+
+import com.google.common.hash.Hashing;
+
 public class EncryptPassword {
     public static String buildEncryptedPassword(String password) {
-       return password;
+        String hashPassword = Hashing.sha256()
+        .hashString(password, StandardCharsets.UTF_8)
+        .toString();
+       
+        return hashPassword;
     }
     public static boolean verifyPasswords(String providedPassword, String securedPassword)
     {
         boolean returnValue = false;
         
-        String newSecurePassword = providedPassword;
+        String newSecurePassword = buildEncryptedPassword(providedPassword);
 
         returnValue = newSecurePassword.equalsIgnoreCase(securedPassword);
         
